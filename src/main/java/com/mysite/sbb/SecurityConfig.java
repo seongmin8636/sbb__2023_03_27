ca835c4f53272bc64458c46cc445045a3d4a5349
seongmin8636 <moss3004@gmail.com>
2023-03-28 15:33:38 +0900

    login,logout

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -2,15 +2,40 @@ package com.mysite.sbb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(
+                        formLogin -> formLogin
+                                // GET
+                                // 시큐리티에게 우리가 만든 로그인 페이지 url을 알려준다.
+                                // 만약에 하지 않으면 기본 로그인 페이지 url은 /login 이다.
+                                .loginPage("/user/login")
+                                .defaultSuccessUrl("/")
+                )
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/user/logout")
+                                .logoutSuccessUrl("/")
+                                .invalidateHttpSession(true)
+                );
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/mysite/sbb/user/UserService.java
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,20 +1,32 @@
 package com.mysite.sbb.user;
 
+import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    private final PasswordEncoder passwordEncoder;
 
     private final UserRepository userRepository;
+
+    public SiteUser getUser(String username) {
+        Optional<SiteUser> siteUser = userRepository.findByUsername(username);
+        if (siteUser.isPresent()) {
+            return siteUser.get();
+        } else {
+            throw new DataNotFoundException("siteuser not found");
+        }
+    }
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         userRepository.save(user);
         return user;
